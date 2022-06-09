930c430858677fdca2569d6db7baf38487924ac5
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-06-08 22:50:00 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#8257e6",
+      },
+      fontFamily: {
+        sans: ["Lexend Deca", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
